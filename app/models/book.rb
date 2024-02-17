0ace9ce270479e764081e6babc9be5493ec21b37
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-17 13:11:34 +0430

    build many to many relationship through favorite_book table

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :favorite_books, dependent: :destroy
+  has_many :users, through: :favorite_books
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :favorite_books, dependent: :destroy
+  has_many :books, through: :favorite_books
   has_many :books
 end
