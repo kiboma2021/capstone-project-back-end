afa75944649f34b56c960ca324ff12c1f898c3e6
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-17 12:15:49 +0430

    removes null:false option from favorite table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_17_070728) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_17_073614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_17_070728) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
+  create_table "favorite_books", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_favorite_books_on_book_id"
+    t.index ["user_id"], name: "index_favorite_books_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "text"
@@ -36,4 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_17_070728) do
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "favorite_books", "books"
+  add_foreign_key "favorite_books", "users"
 end
