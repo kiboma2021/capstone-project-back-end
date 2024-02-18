cac019f528d93e3cccd6b618d0330ba8b597281c
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-18 18:37:53 +0430

    update book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_one_attached :image
+  has_one_attached :image,dependent: :destroy
   belongs_to :user
   has_many :favorite_books, dependent: :destroy
   has_many :users, through: :favorite_books
