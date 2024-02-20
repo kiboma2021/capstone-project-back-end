81f806077ba2528ebd29ea48e35eaa30fc5475ca
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-20 11:37:44 +0430

    fix linter

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,9 +8,12 @@ RSpec.describe Book, type: :model do
     it { should validate_presence_of(:author) }
     it { should validate_presence_of(:year) }
     it { should validate_numericality_of(:year).only_integer.is_greater_than(1950) }
-    it {
-      should validate_numericality_of(:rating).only_integer.is_greater_than_or_equal_to(0).is_less_than_or_equal_to(5).allow_nil
-    }
+    it { should validate_numericality_of(:rating)
+    .only_integer
+    .is_greater_than_or_equal_to(0)
+    .is_less_than_or_equal_to(5)
+    .allow_nil }
+
   end
 end
 RSpec.describe Book, type: :model do
