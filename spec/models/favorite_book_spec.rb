479238cc6d302d44304c26dfca409bc6ece6025f
ZabihullahNooriWardak <115200857+ZabihullahNooriWardak@users.noreply.github.com>
2024-02-20 11:26:21 +0430

    adds associations test for favorit-book model

--- spec/models/favorite_book_spec.rb
+++ spec/models/favorite_book_spec.rb
@@ -6,3 +6,9 @@ RSpec.describe FavoriteBook, type: :model do
     it { should validate_presence_of(:book_id) }
   end
 end
+RSpec.describe FavoriteBook, type: :model do
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:book) }
+  end
+end
