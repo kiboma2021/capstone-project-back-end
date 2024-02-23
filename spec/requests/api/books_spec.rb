86c0c4d22295219360ac5fd6bcc238f50e033e77
Gumaro Monroy <maromvz@gmail.com>
2024-02-23 17:41:41 -0600

    Setup swagger and added documentation tests for api

---
+++ spec/requests/api/books_spec.rb
@@ -0,0 +1,118 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/books', type: :request do
+  let(:user) { User.create!(
+      name: 'username',
+      email: 'example@example.com',
+      password: 'password'
+    ) 
+  }
+
+  path '/books' do
+    post 'Creates a book' do
+      tags 'Books'
+      consumes 'application/json'
+      parameter name: :book, in: :body, schema: {
+        type: :object,
+        properties: {
+          user_id: { type: :integer },
+          title: { type: :string },
+          author: { type: :string },
+          price: { type: :number },
+          year: { type: :integer },
+          rating: { type: :number },
+          image_url: { type: :string },
+          description: { type: :string }
+        },
+        required: %w[user_id title author price year rating description]
+      }
+
+      response '200', 'Book created successfully' do
+        let(:book) {
+          {
+            title: 'Book Title',
+            author: 'Book Author',
+            price: 9.99,
+            year: 2020,
+            rating: 5,
+            description: 'Book Description',
+            image_url: 'book_image.jpg'
+          }
+        }
+        run_test!
+      end
+
+      response '500', 'Book could not be saved' do
+        let(:status) {
+          {
+            code: 500,
+            message: "Book couldn't be saved. [+ error messages.]"
+          }
+        }
+        run_test!
+      end
+    end
+
+    get 'Retrieves all books' do
+      tags 'Books'
+      produces 'application/json'
+
+      response '200', 'Books retrieved' do
+        let(:books) {
+          []
+        }
+        run_test!
+      end
+    end
+  end
+
+  path '/books/{id}' do
+    get 'Retrieves a book' do
+      tags 'Books'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer
+
+      response '200', 'Book retrieved' do
+        let(:book) {
+          Book.new(
+            title: 'Moby Dick',
+            author: 'Herman Melville',
+            description: 'About whales',
+            price: 5,
+            rating: 4,
+            user: user,
+            year: 1920
+          )
+        }
+        run_test!
+      end
+    end
+
+    delete 'Deletes a book' do
+      tags 'Books'
+      produces 'application/json'
+      parameter name: :id, in: :path, type: :integer
+
+      response '200', 'Book deleted' do
+        let(:response) {
+          {
+            message: 'Book deleted'
+          }
+        }
+        run_test!
+      end
+      
+      response '422', 'Book couldn\'t be deleted' do
+        let(:response) {
+          {
+            message: '[error messages]',
+            status: {
+              code: 422
+            }
+          }
+        }
+        run_test!
+      end
+    end
+  end
+end
--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -24,15 +24,17 @@ RSpec.describe 'api/users', type: :request do
             email: 'username@example.com'
           }
         }
+        run_test!
       end
 
       response '422', "User couldn't be created successfully." do
         let(:status) {
           {
             code: 422,
-            message "User couldn't be created successfully. [+ error messages.]"
+            message: "User couldn't be created successfully. [+ error messages.]"
           }
         }
+        run_test!
       end
     end
 
@@ -42,24 +44,96 @@ RSpec.describe 'api/users', type: :request do
       parameter name: :user, in: :body, schema: {
         type: :object,
         properties: {
-          name: 'username',
-          email: 'username@example.com',
-          password: 'password123'
+          name: { type: :string },
+          email: { type: :string },
+          password: { type: :string }
         }
       }
 
       response '200', 'Account deleted successfully' do
         let(:status) {
-          code: 200,
-          message: 'Account deleted successfully.'
+          {
+            code: 200,
+            message: 'Account deleted successfully.'
+          }
         }
+        run_test!
       end
 
       response '422', "Account couldn't be deleted successfully." do
         let(:status) {
-          code: 422,
-          message: "Account couldn't be deleted successfully."
+          {
+            code: 422,
+            message: "Account couldn't be deleted successfully."
+          }
+        }
+        run_test!
+      end
+    end
+  end
+
+  path '/login' do
+    post 'Logs in the user' do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string},
+          password: { type: :string}
+        },
+        required: %w[email password]
+      }
+
+      response '200', 'User logged in successfully' do
+        let(:response) {
+          {
+            status: {
+              code: 200, message: 'Logged in successfully'
+            },
+            data: {
+              id: 1,
+              name: "Username",
+              email: 
+            }
+          }
+        }
+        run_test!
+      end
+
+      response '422', 'User could not log in' do
+        let(:response) {
+          {
+            code: 422,
+            message: "User couldn't log in. [error messages]"
+          }
         }
       end
     end
+
+    delete 'Deletes user' do
+      tags 'Users'
+      parameter name: :auth_token, in: :header, type: :string
+
+      response '200', 'Logged out successfully' do
+        let(:respoonse) {
+          {
+            status: 200,
+            message: "logged out successfully",
+          }
+        }
+        run_test!
+      end
+
+      response '401', 'Could not find an active session' do
+        let(:response) {
+          {
+            status: 401,
+            message: 'Could not find an active session'
+          }
+        }
+        run_test!
+      end
+    end
+  end
 end
