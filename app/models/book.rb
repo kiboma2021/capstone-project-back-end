b3552d591b4f0aab06784651529ca939fdf3faec
Gumaro Monroy <maromvz@gmail.com>
2024-02-23 14:42:06 -0600

    Patch models tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,13 +2,13 @@ class Book < ApplicationRecord
   has_one_attached :image, dependent: :destroy
   belongs_to :user
   has_many :favourites, dependent: :destroy
-  has_many :users, through: :favorites
+  has_many :users, through: :favourites
   validates :title, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :author, presence: true
   validates :description, presence: true, length: { maximum: 70 }
   validates :year, presence: true, numericality: { only_integer: true, greater_than: 1000 }
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 
   def image_url
     Rails.application.routes.url_helpers.url_for(image) if image.attached?
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,20 +7,18 @@ RSpec.describe Book, type: :model do
     it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
     it { should validate_presence_of(:author) }
     it { should validate_presence_of(:year) }
-    it { should validate_numericality_of(:year).only_integer.is_greater_than(1950) }
+    it { should validate_numericality_of(:year).only_integer.is_greater_than(1000) }
     it {
       should validate_numericality_of(:rating)
         .only_integer
         .is_greater_than_or_equal_to(0)
         .is_less_than_or_equal_to(5)
-        .allow_nil
     }
   end
-end
-RSpec.describe Book, type: :model do
+
   describe 'associations' do
     it { should belong_to(:user) }
-    it { should have_many(:favorite_books).dependent(:destroy) }
-    it { should have_many(:users).through(:favorite_books) }
+    it { should have_many(:favourites).dependent(:destroy) }
+    it { should have_many(:users).through(:favourites) }
   end
 end
